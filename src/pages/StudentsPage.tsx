0562833d2ce22b8f1e42f2e0926464143764f414
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-09-18 20:39:47 +0100

    Created logic for SearchBar in StudenPage.tsx
    Added SearchBar.tsx

--- src/pages/StudentsPage.tsx
+++ src/pages/StudentsPage.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import StudentList from "../components/StudentList";
 import studentsData from '../data/students.json';
+import SearchBar from '../components/SearchBar';
 
 interface StudentProps {
     id: number;
@@ -13,10 +14,33 @@ interface StudentProps {
 const StudentsPage:React.FC = () => {
 
     const [students, setStudents] = useState<StudentProps[]>(studentsData);
+    const [foundStudents, setFoundStudents] = useState<{ name: string; form: string; send: boolean }[] | null>(null);
+
+    const handleSearch = (term: string) => {
+
+        if (term.trim() === '') {
+            setFoundStudents([]); // Clear results if search term is empty
+          } else {
+            const filteredStudents = students.filter(
+              (student) =>
+                `${student.forename} ${student.surname} ${student.form}`
+                  .toLowerCase()
+                  .includes(term.toLowerCase())
+            );
+      
+            setFoundStudents(filteredStudents.map(student => ({
+              name: `${student.forename} ${student.surname}`,
+              form: student.form,
+              send: student.send
+            })));
+          }
+        };
+    
 
     return ( 
         <div className="StudentsPage">
             <h2>All pupils</h2>
+            <SearchBar onSearch={handleSearch} foundStudents={foundStudents} />
             <StudentList students={students} />
         </div>
      );
