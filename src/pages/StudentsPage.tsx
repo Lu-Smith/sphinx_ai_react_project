4783a38344c1dcabc9f709fe665eb47a5137d366
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-09-18 19:52:46 +0100

    Updated StudentsPage.tsx - pass props to StudentList.tsx

--- src/pages/StudentsPage.tsx
+++ src/pages/StudentsPage.tsx
@@ -1,10 +1,23 @@
+import React, { useState } from 'react';
 import StudentList from "../components/StudentList";
+import studentsData from '../data/students.json';
 
-const StudentsPage = () => {
+interface StudentProps {
+    id: number;
+    forename: string;
+    surname: string;
+    form: string;
+    send: boolean;
+}
+
+const StudentsPage:React.FC = () => {
+
+    const [students, setStudents] = useState<StudentProps[]>(studentsData);
+
     return ( 
         <div className="StudentsPage">
             <h2>All pupils</h2>
-            <StudentList />
+            <StudentList students={students} />
         </div>
      );
 }
