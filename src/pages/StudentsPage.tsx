de1fb3fd3bccca3c150b8d2a9a2be9d60e710ae1
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-09-18 21:42:44 +0100

    Started make the table more dynamic in StudentsPage.tsx
    - added columns props and passed to StudenList.tsx

--- src/pages/StudentsPage.tsx
+++ src/pages/StudentsPage.tsx
@@ -11,32 +11,43 @@ interface StudentProps {
     send: boolean;
 }
 
+interface Column {
+  key: keyof StudentProps;
+  label: string;
+}
+
 const StudentsPage:React.FC = () => {
 
     const [filteredStudents, setFilteredStudents] = useState<StudentProps[]>(studentsData);
 
+    const columns: Column[] = [
+      { key: 'forename', label: "Forename" },
+      { key: 'surname', label: "Surname" },
+      { key: 'form', label: "Form" },
+      { key: 'send', label: "SEND" }
+    ];
+
     const handleSearch = (term: string) => {
 
-        if (term.trim() === '') {
-          setFilteredStudents(studentsData); 
-          } else {
-            const filtered = studentsData.filter(
-              (student) =>
-                `${student.forename} ${student.surname} ${student.form}`
-                  .toLowerCase()
-                  .includes(term.toLowerCase())
-            );
-            setFilteredStudents(filtered);
-          }
-        };
+      if (term.trim() === '') {
+        setFilteredStudents(studentsData); 
+        } else {
+          const filtered = studentsData.filter(
+            (student) =>
+              `${student.forename} ${student.surname} ${student.form}`
+                .toLowerCase()
+                .includes(term.toLowerCase())
+          );
+          setFilteredStudents(filtered);
+        }
+    };
     
-
     return ( 
-        <div className="StudentsPage">
-            <h2>All pupils</h2>
-            <SearchBar onSearch={handleSearch} />
-            <StudentList students={filteredStudents} />
-        </div>
+      <div className="StudentsPage">
+          <h2>All pupils</h2>
+          <SearchBar onSearch={handleSearch} />
+          <StudentList students={filteredStudents} columns={columns} />
+      </div>
      );
 }
 
