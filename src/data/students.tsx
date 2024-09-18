83803a6996b5ffcf62782f71f4df5e42657f1d6a
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-09-18 22:51:25 +0100

    Changed students.json into students.tsx

--- src/data/students.tsx
+++ src/data/students.tsx
@@ -1,107 +1,132 @@
-[
+import Avatar1 from '../assets/Avatar1.jpg';
+import Avatar2 from '../assets/Avatar2.jpg';
+import Avatar3 from '../assets/Avatar3.jpg';
+import Avatar4 from '../assets/Avatar4.jpg';
+import Avatar5 from '../assets/Avatar5.jpg';
+import Avatar6 from '../assets/Avatar6.jpg';
+import Avatar7 from '../assets/Avatar7.jpg';
+
+const students = [
   {
     "id": 1,
     "forename": "Harry",
     "surname": "Potter",
     "form": "Gryffindor",
-    "send": true
+    "send": true,
+    "src": Avatar1
   },
   {
     "id": 2,
     "forename": "Hermione",
     "surname": "Granger",
     "form": "Gryffindor",
-    "send": false
+    "send": false,
+    "src": Avatar2
   },
   {
     "id": 3,
     "forename": "Ron",
     "surname": "Weasley",
     "form": "Gryffindor",
-    "send": true
+    "send": true,
+    "src": Avatar3
   },
   {
     "id": 4,
     "forename": "Draco",
     "surname": "Malfoy",
     "form": "Slytherin",
-    "send": false
+    "send": false,
+    "src": Avatar4
   },
   {
     "id": 5,
     "forename": "Luna",
     "surname": "Lovegood",
     "form": "Ravenclaw",
-    "send": true
+    "send": true,
+    "src": Avatar5
   },
   {
     "id": 6,
     "forename": "Neville",
     "surname": "Longbottom",
     "form": "Gryffindor",
-    "send": false
+    "send": false,
+    "src": Avatar6
   },
   {
     "id": 7,
     "forename": "Ginny",
     "surname": "Weasley",
     "form": "Gryffindor",
-    "send": true
+    "send": true,
+    "src": Avatar7
   },
   {
     "id": 8,
     "forename": "Cedric",
     "surname": "Diggory",
     "form": "Hufflepuff",
-    "send": true
+    "send": true,
+    "src": Avatar1
   },
   {
     "id": 9,
     "forename": "Cho",
     "surname": "Chang",
     "form": "Ravenclaw",
-    "send": false
+    "send": false,
+    "src": Avatar2
   },
   {
     "id": 10,
     "forename": "Pansy",
     "surname": "Parkinson",
     "form": "Slytherin",
-    "send": false
+    "send": false,
+    "src": Avatar3
   },
   {
     "id": 11,
     "forename": "Dean",
     "surname": "Thomas",
     "form": "Gryffindor",
-    "send": false
+    "send": false,
+    "src": Avatar4
   },
   {
     "id": 12,
     "forename": "Seamus",
     "surname": "Finnigan",
     "form": "Gryffindor",
-    "send": false
+    "send": false,
+    "src": Avatar5
   },
   {
     "id": 13,
     "forename": "Lavender",
     "surname": "Brown",
     "form": "Gryffindor",
-    "send": true
+    "send": true,
+    "src": Avatar6
   },
   {
     "id": 14,
     "forename": "Blaise",
     "surname": "Zabini",
     "form": "Slytherin",
-    "send": false
+    "send": false,
+    "src": Avatar7
   },
   {
     "id": 15,
     "forename": "Fleur",
     "surname": "Delacour",
     "form": "Beauxbatons",
-    "send": true
+    "send": true,
+    "src": Avatar1
   }
 ]
+
+export default students;
