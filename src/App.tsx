4357f4e30956a7d943c39a288da4864bc7395eaf
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-09-18 21:01:16 +0100

    Added ExplanationPage to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import StudentsPage from './pages/StudentsPage';
+import ExplanationPage from './pages/ExplanationPage';
 
 const App: React.FC = () => {
 
@@ -11,10 +12,12 @@ const App: React.FC = () => {
         <nav>
           <Link to="/">Home</Link>
           <Link to="/student">Students</Link>
+          <Link to="/explanation">Explanation</Link>
         </nav>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/student" element={<StudentsPage />} />
+          <Route path="/explanation" element={<ExplanationPage />} />
         </Routes>
       </div>
     </Router>
