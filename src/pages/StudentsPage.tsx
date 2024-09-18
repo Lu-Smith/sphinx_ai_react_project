b25e720b4b4a69aaa3d8176d6e4930ba7426f799
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-09-18 18:53:26 +0100

    Created StudentsPage.tsx

---
+++ src/pages/StudentsPage.tsx
@@ -0,0 +1,9 @@
+const StudentsPage = () => {
+    return ( 
+        <div className="StudentsPage">
+            <h2>I am Students Page</h2>
+        </div>
+     );
+}
+
+export default StudentsPage;
