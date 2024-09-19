8ea3331a75c1506f616b1193847b14b530f7f1e4
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-09-19 08:14:51 +0100

    Added animation (framer-motion) to HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,15 @@
-const HomePage = () => {
+import React from 'react';
+import { motion } from 'framer-motion';
+
+const HomePage:React.FC = () => {
     return ( 
         <div className="HomePage">
-            <h1>Welcome <span>to the friendly</span> Student Managment Hub</h1>
+            <motion.h1
+            initial={{ scale: 0.8, opacity: 0 }}
+            animate={{ scale: 1, opacity: 1 }}    
+            transition={{ duration: 0.8 }}>
+                Welcome <span>to the friendly</span> Student Managment Hub
+            </motion.h1>
             <p>Empowering students to achieve their potential with AI support, while making life easier for teachers!</p>
         </div>
      );
