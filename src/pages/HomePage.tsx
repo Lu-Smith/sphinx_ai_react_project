d0364f52ebbac4c5e16ba5bbaa137b9189f4760a
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-09-19 07:47:33 +0100

    Updated HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,7 @@
 const HomePage = () => {
     return ( 
         <div className="HomePage">
-            <h1>Welcome to the friendly Student Managment Hub</h1>
+            <h1>Welcome <span>to the friendly</span> Student Managment Hub</h1>
             <p>Empowering students to achieve their potential with AI support, while making life easier for teachers!</p>
         </div>
      );
